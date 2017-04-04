122bbea3d817096e9aa97e5dc0c67b0899af6bc1
djallred <david_allred@byu.edu>
2017-04-03 18:40:52 -0600

    Adding FB for Input and channel

--- SPlsWork/SimplSharpStringUtils.h
+++ SPlsWork/SimplSharpStringUtils.h
@@ -1,6 +1,23 @@
 namespace SimplSharpStringUtils;
         // class declarations
+         class JsonExtensions;
          class StringUtils;
+    static class JsonExtensions 
+    {
+        // class delegates
+
+        // class events
+
+        // class functions
+        STRING_FUNCTION ToString ();
+        SIGNED_LONG_INTEGER_FUNCTION GetHashCode ();
+
+        // class variables
+        INTEGER __class_id__;
+
+        // class properties
+    };
+
      class StringUtils 
     {
         // class delegates
@@ -8,10 +25,10 @@ namespace SimplSharpStringUtils;
         // class events
 
         // class functions
-        STRING_FUNCTION ip ( STRING iptmp );
-        STRING_FUNCTION HexToAscii ( STRING hexString );
         STRING_FUNCTION JSONAttributes ( STRING body , STRING desiredAttribute );
-        STRING_FUNCTION getSonyResponseValue ( STRING body , STRING desiredAttribute );
+        SIGNED_LONG_INTEGER_FUNCTION NumberOfMembers ( STRING stringToParse , STRING separatingcharacter );
+        STRING_FUNCTION GetMemberOfStringArray ( STRING stringToParse , STRING separatingcharacter , SIGNED_LONG_INTEGER positionToReturn );
+        STRING_FUNCTION SonyResult ( STRING body , STRING desiredAttribute );
         STRING_FUNCTION ToString ();
         SIGNED_LONG_INTEGER_FUNCTION GetHashCode ();
 
